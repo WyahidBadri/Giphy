fb61eef7866f416c5346da08fe8df5a368d267c6
Wyahid Badri <wyahidb2015@gmail.com>
2019-06-10 09:20:05 -0400

    first commit

---
+++ src/giphyResults.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class GiphyResults extends React.Component {
+
+  render() {
+    return (
+      <div>
+          
+      </div>
+    );
+  }
+}
+
+export default GiphyResults;
